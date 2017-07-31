01d599fd34e675002472232c67e399dfbefb3e74
Andrew Michael Potter <i.am.andrew.potter@gmail.com>
2017-07-31 12:17:54 -0700

    begin work on films.test.js

---
+++ tests/e2e/films.test.js
@@ -0,0 +1,143 @@
+const db = require('./_db');
+const request = require('./_request');
+const assert = require('chai').assert;
+
+describe('films route', () => {
+    before(db.drop);
+
+    it('initial GET returns empty list', () => {
+        return request.get('/films')
+            .then(req => {
+                const films = req.body;
+                assert.deepEqual(films, []);
+            });
+    });
+
+    // Create Films, maybe also create studios and actors
+
+    // Studios
+    let disney = {
+        name: 'Disney',
+        city: 'Orlando',
+        state: 'Florida',
+        country: 'USA'
+    };
+
+    let universal = {
+        name: 'Universal Studios',
+        city: 'Los Angeles',
+        state: 'California',
+        country: 'USA'
+    };
+
+    // Actors
+    let tom = {
+        name: 'Tom Hanks',
+        dob: 1956-07-09,
+        pob: 'Concord, CA'
+    };
+
+    let adam = {
+        name: 'Adam Sandler',
+        dob: 1966-09-09,
+        pob: 'Brooklyn, NY'
+    };
+
+    let marilyn = {
+        name: 'Marilyn Monroe',
+        dob: 1926-06-01,
+        pob: 'Los Angeles, CA'
+    };
+
+    // Films
+    let savingPrivateRyan = {
+        title: 'Saving Private Ryan',
+        studio: universal._id,
+        released: 2003,
+        cast: [tom._id, adam._id]
+    };
+
+    let incredibles = {
+        title: 'The Incredibles',
+        studio: disney._id,
+        released: 2004,
+        cast: [adam._id, marilyn._id]
+    };
+
+    let hulk = {
+        title: 'The Incredible Hulk',
+        studio: disney._id,
+        released: 1987,
+        cast: [tom._id, marilyn._id]
+    };
+
+    function saveActor(actor) {
+        return request
+            .post('/actors')
+            .send(actor)
+            .then(res => res.body);
+    }
+
+    saveActor(tom);
+    saveActor(adam);
+    saveActor(marilyn);
+
+    function saveStudio(studio) {
+        return request
+            .post('/studios')
+            .send(studio)
+            .then(res => res.body);
+    }
+
+    saveStudio(disney);
+    saveStudio(universal);
+
+    function saveFilm(film){
+        return request
+            .post('/films')
+            .send(film)
+            .then(res => res.body);
+    }
+
+    it('roundtrips a new film', () => {
+        return saveFilm(savingPrivateRyan)
+            .then(saved => {
+                assert.ok(saved._id, 'saved has id');
+                savingPrivateRyan = saved;
+                console.log(saved);
+            })
+            .then(() => {
+                return request.get(`/films/${savingPrivateRyan._id}`);
+            })
+            .then(res => res.body)
+            .then(got => {
+                assert.deepEqual(got, savingPrivateRyan);
+            });
+    });
+
+    it('GET returns a 404 for nonexistent id', () => {
+        const nonId = '589d04a8b6695bbdfd3106f1';
+        return request.get(`/films/${nonId}`)
+            .then(
+                () => {throw new Error('expected 404');},
+                res => {
+                    assert.equal(res.status, 404);
+                }
+            );
+    });
+
+    it('returns list of all films', () => {
+        return Promise.all([
+            saveFilm(incredibles),
+            saveFilm(hulk)
+        ])
+            .then(savedFilms => {
+                incredibles = savedFilms[1];
+                hulk = savedFilms[2];
+            })
+            .then(() => request.get('/films'))
+            .then(res => res.body)
+            .then()
+    });
+
+});
