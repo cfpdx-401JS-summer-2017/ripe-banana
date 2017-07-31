cade7eb3968e3979dca32429bf118d82006e357d
Anwar Montasir <anwarmontasir@gmail.com>
2017-07-31 12:17:37 -0700

    reviews test does not work

--- tests/e2e/reviews.test.js
+++ tests/e2e/reviews.test.js
@@ -5,6 +5,16 @@ const assert = require('chai').assert;
 describe('reviews route', () => {
     before(db.drop);
 
+    let reviewer = null;
+    let actor1 = null;
+    let actor2 = null;
+    let film = null;
+
+    before(() => {
+        return request.post('/reviewers')
+        
+    });
+
     it('initial GET returns empty list', () => {
         return request.get('/reviews')
             .then(req => {
@@ -12,4 +22,103 @@ describe('reviews route', () => {
                 assert.deepEqual(reviews, []);
             });
     });
+
+    let rogerEbert = {
+        name: 'Roger Ebert',
+        company: 'Chicago Sun Times'
+    };
+
+    function saveReviewer(reviewer) {
+        return request
+            .post('/reviewers')
+            .send(reviewer)
+            .then(res => res.body)
+            .then(data => {
+                reviewer._id = data._id;
+            });
+    }
+
+    saveReviewer(rogerEbert);
+
+    let naomiWatts = {
+        name: 'Naomi Watts',
+        dob: 1968-09-28,
+        pob: 'Shoreham, England'
+    };
+
+    let lauraElenaHarring = {
+        name: 'Laura Elena Harring',
+        dob: 1964-03-03,
+        pob: 'Los Mochis, Mexico'
+    };
+
+    function saveActor(actor) {
+        return request
+            .post('/actors')
+            .send(actor)
+            .then(res => res.body)
+            .then(data => {
+                actor._id = data._id;
+                console.log('ACTOR!!!', actor);
+            });
+    }
+
+    saveActor(naomiWatts);
+    saveActor(lauraElenaHarring);
+    
+    let mulhollandDrive = {
+        title: 'Mulholland Drive',
+        studio: 'Universal Pictures',
+        released: 2001,
+        cast: [
+            naomiWatts._id,
+            lauraElenaHarring._id
+        ]
+    };
+
+    function saveFilm(film) {
+        console.log('*** naomiWatts._id', naomiWatts._id);
+        return request
+            .post('/films')
+            .send(film)
+            .then(res => res.body)
+            .then(data => {
+                film._id = data._id;
+            });
+    }
+
+    saveFilm(mulhollandDrive);
+    
+    let mulhollandDriveReview = {
+        rating: 5,
+        reviewer: rogerEbert._id,
+        content: 'David Lynch has been working toward Mulholland Drive all of his career, and now that he’s arrived there I forgive him Wild at Heart and...',
+        film: mulhollandDrive._id
+    };
+
+    function saveReview(review) {
+        return request
+            .post('/reviews')
+            .send(review)
+            .then(res => res.body);
+    }
+
+    it('roundtrips a new review', () => {
+        console.log('*** rogerEbert._id', rogerEbert._id);
+        console.log('*** mulhollandDrive._id', mulhollandDrive._id);
+        return saveReview(mulhollandDriveReview)
+            .then(saved => {
+                assert.ok(saved._id, 'saved has id');
+                mulhollandDriveReview = saved;
+            })
+            .then(() => {
+                return request.get(`/reviews/${mulhollandDriveReview._id}`);
+            })
+            .then(res => res.body)
+            .then(got => {
+                assert.deepEqual(got, mulhollandDriveReview);
+            });
+    });
+
+    after(db.drop);
 });
