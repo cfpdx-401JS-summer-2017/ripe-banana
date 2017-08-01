0c1ee7dd911074fc789d176bca4add51568914cf
Haley Tortorella <hdtort@gmail.com>
2017-08-01 11:52:48 -0700

    passing tests for reviews

--- tests/e2e/reviews.test.js
+++ tests/e2e/reviews.test.js
@@ -18,6 +18,21 @@ describe('reviews route', () => {
         company: 'Chicago Sun Times'
     };
 
+    let banwarTheSanwar = {
+        name: 'The Banwar of Sanwar',
+        company: 'Hugedorks.com'
+    };
+
+    let haleyHales = {
+        name: 'Haley Hales',
+        company: 'Alchemy Code Lab'
+    };
+
+    let mandrewMotterTheOtter = {
+        name: 'Mandrew Motter The Otter',
+        company: 'Portland Otter Factorie'
+    };
+
     let naomiWatts = {
         name: 'Naomi Watts',
         dob: new Date('1968-09-28'),
@@ -131,5 +146,98 @@ describe('reviews route', () => {
             .then(got => {
                 assert.deepEqual(got, mulhollandDriveReview);
             });
+    });
+
+    it('GET returns 404 for non-existent id', () => {
+        const nonId = '589d04a8b6695bbdfd3106f1';
+        return request.get(`/reviews/${nonId}`)
+            .then(
+                () => {throw new Error('expected 404');},
+                res => {
+                    assert.equal(res.status, 404);
+                }
+            );
+    });
+
+    it('returns list of all reviews', () => {
+        let mulhollandDriveReview2 = {
+            rating: 3,
+            reviewer: haleyHales._id,
+            content: 'Not enough cats.',
+            film: mulhollandDrive._id
+        };
+        let mulhollandDriveReview3 = {
+            rating: 2,
+            reviewer: mandrewMotterTheOtter._id,
+            content: 'I did not see it.',
+            film: mulhollandDrive._id
+        };
+
+        return Promise.all([
+            saveReview(mulhollandDriveReview2),
+            saveReview(mulhollandDriveReview3)
+        ])
+            .then(savedReview => {
+                mulhollandDriveReview2 = savedReview[1];
+                mulhollandDriveReview3 = savedReview[2];
+            })
+            .then(() => request.get('/reviews'))
+            .then(res => res.body)
+            .then(review => {
+                assert.isTrue(review.length > 1);
+            });
+
+    });
+
+    it('updates a review', () => {
+        let mulhollandDriveReview4 = {
+            rating: 1,
+            reviewer: banwarTheSanwar._id,
+            content: 'Sanwars do not have eyes.',
+            film: mulhollandDrive._id
+        };
+        saveReview(mulhollandDriveReview4)
+            .then( () => {
+                mulhollandDriveReview4.rating = 5;
+                return request.put(`reviews/${mulhollandDriveReview4._id}`)
+                    .send(mulhollandDriveReview4)
+                    .then(res => res.body)
+                    .then( updated => {
+                        assert.equal(updated.rating, mulhollandDriveReview4.rating);
+                    });
+            });
+    });
+
+    it('deletes a review', () => {
+        let mulhollandDriveReview5 = {
+            rating: 4,
+            reviewer: banwarTheSanwar._id,
+            content: 'There has been an awful mistake',
+            film: mulhollandDrive._id
+        };
+        saveReview(mulhollandDriveReview5)
+            .then( () => {
+                return request.delete(`/reviews/${mulhollandDriveReview5._id}`)
+                    .then( res => res.body)
+                    .then(result => {
+                        assert.isTrue(result.removed);
+                    });
+            });
+    });
+
+    it('deletes a non-existent studio removed false', () => {
+        return request.delete('/reviews/589d04a8b6695bbdfd3106f1')
+            .then(res => res.body)
+            .then(result => {
+                assert.isFalse(result.removed);
+            });
+    });
+
+    it('errors on validation failure', () => {
+        return saveReview({})
+            .then(
+                () => { throw new Error('expected failure');},
+                () => { }
+            );
     });
 });
